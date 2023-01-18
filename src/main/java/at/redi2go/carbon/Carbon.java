29ef8e9b8ced1f24354c1efa3dbff014f68cd9d6
Lukas Gebetsberger <20180009@students.htl-perg.ac.at>
2023-01-18 16:11:57 +0100

    Initial commit

---
+++ src/main/java/at/redi2go/carbon/Carbon.java
@@ -0,0 +1,10 @@
+package at.redi2go.carbon;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Carbon implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/at/redi2go/carbon/client/CarbonClient.java
@@ -0,0 +1,13 @@
+package at.redi2go.carbon.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class CarbonClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
